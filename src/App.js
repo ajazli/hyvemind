de38545ab17d9c5cb536ce975a923a093736c7b6
ajazli <50915513+ajazli@users.noreply.github.com>
2023-06-18 09:45:54 +0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './h-m.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <br>
+        <p>
+          HHYVEMIND
+        </p>
+        <p>
+          watch this space..
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
